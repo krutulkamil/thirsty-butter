fd8c78a9c5df700aad994b7a0ceed1f2971d0444
krutulkamil <krutulkamil@onet.pl>
2024-02-11 16:37:38 +0100

    feat: dashboard layout init

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { UserButton } from '@clerk/nextjs';
+
+import { ThemeSwitcher } from '@/components/layout/theme-switcher';
+import { Logo } from '@/components/layout/logo';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({
+  children,
+}: Readonly<DashboardLayoutProps>) {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+        <Logo />
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+}
