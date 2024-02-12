10018170228ceb79121c987a1571f6f64bdee71c
krutulkamil <krutulkamil@onet.pl>
2024-02-12 20:47:32 +0100

    feat: published form view

--- components/form-builder/form-builder.tsx
+++ components/form-builder/form-builder.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import {
   DndContext,
   MouseSensor,
@@ -9,6 +10,8 @@ import {
   useSensors,
 } from '@dnd-kit/core';
 import { ImSpinner2 } from 'react-icons/im';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+import Confetti from 'react-confetti';
 import type { Form } from '@prisma/client';
 
 import { PreviewDialogButton } from '@/components/buttons/preview-dialog-button';
@@ -17,6 +20,9 @@ import { PublishFormButton } from '@/components/buttons/publish-form-button';
 import { Designer } from '@/components/form-builder/designer';
 import { DragOverlayWrapper } from '@/components/form-builder/drag-overlay-wrapper';
 import { useDesigner } from '@/components/hooks/use-designer';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
 
 interface FormBuilderProps {
   form: Form;
@@ -60,6 +66,60 @@ export function FormBuilder({ form }: Readonly<FormBuilderProps>) {
       </div>
     );
   }
+
+  const shareUrl = `${window.location.origin}/submit/${form.shareURL}`;
+
+  if (form.published)
+    return (
+      <>
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+          numberOfPieces={1000}
+        />
+        <div className="flex flex-col items-center justify-center h-full w-full">
+          <div className="max-w-md">
+            <h1 className="text-center text-4xl font-bold text-primary border-b pb-2 mb-10">
+              🎊 Form Published 🎊
+            </h1>
+            <h2 className="text-2xl">Share this form</h2>
+            <h3 className="text-xl text-muted-foreground border-b pb-10">
+              Anyone with the link can view and submit the form
+            </h3>
+            <div className="my-4 flex flex-col gap-2 items-center w-full border-b pb-4">
+              <Input className="w-full" readOnly value={shareUrl} />
+              <Button
+                className="mt-2 w-full"
+                onClick={() => {
+                  navigator.clipboard.writeText(shareUrl);
+                  toast({
+                    title: 'Copied!',
+                    description: 'Link copied to clipboard',
+                  });
+                }}
+              >
+                Copy link
+              </Button>
+            </div>
+            <div className="flex justify-between">
+              <Button variant={'link'} asChild>
+                <Link href={'/'} className="gap-2">
+                  <BsArrowLeft />
+                  Go back home
+                </Link>
+              </Button>
+              <Button variant={'link'} asChild>
+                <Link href={`/forms/${form.id}`} className="gap-2">
+                  Form details
+                  <BsArrowRight />
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 
   return (
     <DndContext sensors={sensors}>
