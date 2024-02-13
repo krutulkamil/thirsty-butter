4d8b6f59dbbf900281055e4a0dab02725a3e4c8d
krutulkamil <krutulkamil@onet.pl>
2024-02-13 21:00:08 +0100

    feat: custom rows for checkbox & date field

--- components/table/row-cell.tsx
+++ components/table/row-cell.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import React from 'react';
+import { format } from 'date-fns';
 
 import { TableCell } from '@/components/ui/table';
+import { Badge } from '@/components/ui/badge';
+import { Checkbox } from '@/components/ui/checkbox';
 import type { ElementsType } from '@/components/form-builder/form-elements';
 
 interface RowCellProps {
@@ -11,7 +14,20 @@ interface RowCellProps {
 }
 
 export function RowCell({ type, value }: Readonly<RowCellProps>) {
-  const node: React.ReactNode = value;
+  let node: React.ReactNode = value;
+
+  switch (type) {
+    case 'DateField':
+      if (!value) break;
+
+      node = (
+        <Badge variant="outline">{format(new Date(value), 'dd/MM/yyyy')}</Badge>
+      );
+      break;
+    case 'CheckboxField':
+      node = <Checkbox checked={value === 'true'} disabled />;
+      break;
+  }
 
   return <TableCell>{node ?? '-'}</TableCell>;
 }
